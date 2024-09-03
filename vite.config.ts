cc4a9d311a775017b84d1eaec645bde886561804
Jethro Baldonado <jethro.baldonado@gmail.com>
2024-09-03 23:35:51 +0800

    add sw

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['vite.svg', 'abc.webp'], // Add other assets if needed
+      manifest: {
+        name: 'My React PWA',
+        short_name: 'ReactPWA',
+        description: 'My Progressive Web App using Vite and React',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            "src": "abc.webp",
+            "sizes": "100x100",
+            "type": "image/webp"
+          },
+          {
+            "src": "vite.svg",
+            "sizes": "100x100",
+            "type": "image/svg+xml"
+          }
+        ],
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#ffffff',
+      }
+    }),
+  ],
 })
